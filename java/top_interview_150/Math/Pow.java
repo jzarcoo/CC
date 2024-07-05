2e677bf16d15d319068847bd68fc4ed49356d5ac
Jose Antonio Zarco Romero <93405714+jzarcoo@users.noreply.github.com>
2024-07-04 22:40:04 -0600

    Update Pow.java

--- java/top_interview_150/Math/Pow.java
+++ java/top_interview_150/Math/Pow.java
@@ -36,9 +36,9 @@ Constraints:
 class Pow {
     public double myPow(double x, int n) {
         if (Double.isNaN(x) || Double.isInfinite(x)) return Double.NaN;
-        if (x == 0) return (n <= 0) ? Double.POSITIVE_INFINITY : 0.0;
+        if (x == 0) return (n <= 0) ? Double.POSITIVE_INFINITY : 0;
         if (n == 0 || x == 1) return 1;
-        if (x == -1.0) return (n % 2 == 0) ? 1.0 : -1.0;
+        if (x == -1) return (n % 2 == 0) ? 1 : -1;
         long N = n; // Integer.MIN_VALUE
         if (N < 0) {
             x = 1 / x;
