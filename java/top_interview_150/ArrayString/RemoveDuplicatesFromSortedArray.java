27cf5c473ce12f45f1e9f004efbf1a5142ce2dea
Jose Antonio Zarco Romero <93405714+jzarcoo@users.noreply.github.com>
2024-07-04 22:30:59 -0600

    Update RemoveDuplicatesFromSortedArray.java

--- java/top_interview_150/ArrayString/RemoveDuplicatesFromSortedArray.java
+++ java/top_interview_150/ArrayString/RemoveDuplicatesFromSortedArray.java
@@ -50,8 +50,6 @@ Constraints:
   */
 class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
-        if (nums.length < 2)
-            return nums.length;
         int a = 0;
         for (int i = 1; i < nums.length; i++)
             if (nums[i] != nums[a])
