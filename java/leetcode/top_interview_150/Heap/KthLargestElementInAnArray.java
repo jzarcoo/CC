74e69886b5496bf86a95c90fd0b73936185b433c
Jose Antonio Zarco Romero <93405714+jzarcoo@users.noreply.github.com>
2024-07-08 21:19:39 -0600

    Update KthLargestElementInAnArray.java

--- java/leetcode/top_interview_150/Heap/KthLargestElementInAnArray.java
+++ java/leetcode/top_interview_150/Heap/KthLargestElementInAnArray.java
@@ -15,11 +15,35 @@
    1 <= k <= nums.length <= 105
    -104 <= nums[i] <= 104
 */
-class Solution {
-    // 23 ms
-    // 57.65 MB
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class KthLargestElementInAnArray {
+
+   // time: O(nlogk), 39ms
+   // space: O(k), 56.78MB
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> q = new PriorityQueue<Integer>(k);
+        // The smallest of the k largest elements is always on the top
+        for (int i = 0; i < k; i++) {
+            q.offer(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > q.peek()) {
+                q.poll();
+                q.offer(nums[i]);
+            }
+        }
+        return q.peek();
+    }
+
+   /**
+    // time: 23 ms O(n^2), promedio O(nlogn)
+    // space: 57.65 MB, O(1)
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
+   */
 }
